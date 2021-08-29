bc04d42aeb63a1b1b594b5ef7109382e9ed0d41e
ssandamali <ssandamali@gmail.com>
2021-08-29 22:46:50 +1200

    Changed front-end functioanlity

--- App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Profile/SelfIntroduction.jsx
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Profile/SelfIntroduction.jsx
@@ -1,21 +1,99 @@
 ﻿/* Self introduction section */
-import React, { Component } from 'react';
-import Cookies from 'js-cookie'
+import React, { Component } from "react";
+import Cookies from "js-cookie";
+import FormItemWrapper from "../Form/FormItemWrapper.jsx";
+import { ChildSingleInput } from "../Form/SingleInput.jsx";
 
 export default class SelfIntroduction extends React.Component {
-    constructor(props) {
-        super(props);
-    
+  constructor(props) {
+    super(props);
+    this.state = {
+      summary: props.summary ? props.summary : "",
+      description: props.description ? pros.description : "",
+      isSummaryError: false,
+      isDescriptionError: false,
     };
 
-    
+    this.updateDescription = this.updateDescription.bind(this);
+    this.saveDescription = this.saveDescription.bind(this);
+  }
 
+  updateDescription(event) {
+    const key = event.target.name;
+    this.setState({
+      [key]: event.target.value,
+    });
+  }
 
+  updateDescription(event) {
+    this.setState({
+      description: event.target.value,
+    });
+  }
 
-    render() {
-       
+  saveDescription() {
+    let isSummaryError = this.state.summary.length === 0 ? true : false;
+    let isDescriptionError = this.state.description.length < 150 ? true : false;
+
+    if (isSummaryError || isDescriptionError) {
+      this.setState({
+        isSummaryError: isSummaryError,
+        isDescriptionError: isDescriptionError,
+      });
+    } else {
+      const data = {
+        summary: this.state.summary,
+        description: this.state.description,
+      };
+      this.setState(
+        {
+          isSummaryError: isSummaryError,
+          isDescriptionError: isDescriptionError,
+        },
+        this.props.saveProfileData(data)
+      );
     }
+  }
+
+  render() {
+    return (
+      <FormItemWrapper title="Description" tooltip="Tell us about yourself">
+        <div className="ui sixteen wide column">
+          <ChildSingleInput
+            inputType="text"
+            name="summary"
+            value={this.props.summary ? this.props.summary : ""}
+            controlFunc={this.updateDescription}
+            maxLength={150}
+            placeholder="Please provide a short summary about yourself"
+            errorMessage="Summary should not be empty"
+            isError={this.state.isSummaryError}
+          />
+          <p>Summary must be no more than 150 charactors</p>
+          <div className="field">
+            <textarea
+              name="description"
+              placeholder="Please tell us about any hobbies, additional expertise or anything you'd like to add."
+              value={this.props.description ? this.props.description : ""}
+              onChange={this.updateDescription}
+              maxLength={600}
+            ></textarea>
+            {this.state.isDescriptionError ? (
+              <div className="ui basic red pointing prompt label transition visible">
+                Description should not be less than 150 characters
+              </div>
+            ) : null}
+          </div>
+          <p>Description much be between 150-600 characters</p>
+          <button
+            type="button"
+            className="ui teal right floated button"
+            onClick={this.saveDescription}
+          >
+            Save
+          </button>
+        </div>
+      </FormItemWrapper>
+    );
+  }
 }
-
-
-
--- App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Profile/TalentVideoSection.jsx
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Profile/TalentVideoSection.jsx
@@ -1,63 +1,59 @@
-﻿import React, { Component } from 'react'
-import VideoUpload from './VideoUpload.jsx'
+﻿import React, { Component } from "react";
+import VideoUpload from "./VideoUpload.jsx";
 
 export class TalentVideoSection extends Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
 
-        this.state = {
+    this.state = {};
 
-        }
+    this.updateForComponentId = this.updateForComponentId.bind(this);
+  }
 
-        this.updateForComponentId = this.updateForComponentId.bind(this)
+  updateForComponentId(data, id) {
+    let newVideos = [];
+    let newValue = true;
+
+    this.props.videos.forEach((x) => console.log(x));
+
+    this.props.videos.forEach(function (x) {
+      if (x.videoName === id) {
+        newVideos.push(Object.assign({}, data));
+        newValue = false;
+      } else {
+        newVideos.push(Object.assign({}, x));
+      }
+    });
+
+    if (newValue) {
+      newVideos.push(Object.assign({}, data));
     }
 
-    updateForComponentId(data, id) {
-        let newVideos = []
-        let newValue = true;
+    this.props.updateProfileData({
+      videos: newVideos,
+    });
 
-        this.props.videos.forEach(x => console.log(x))
-
-        this.props.videos.forEach(function (x) {
-
-            if (x.videoName === id) {
-                newVideos.push(Object.assign({}, data))
-                newValue = false
-            }
-            else {
-                newVideos.push(Object.assign({}, x))
-            }
-        })
-
-        if (newValue) {
-            newVideos.push(Object.assign({}, data))
-        }
-
-        this.props.updateProfileData({
-            videos: newVideos
-        })
-
-        if (this.props.activeVideo === id) {
-            this.props.updateProfileData({
-                activeVideo: data.videoName
-            })
-        }
+    if (this.props.activeVideo === id) {
+      this.props.updateProfileData({
+        activeVideo: data.videoName,
+      });
     }
+  }
 
-    render() {
-        let uploaders = this.props.videos.map(x =>
-            <VideoUpload
-                videoName={this.state.profileData.videoName}
-                updateProfileData={this.updateForComponentId}
-                saveVideoUrl={'http://localhost:60290/profile/profile/updateTalentVideo'}
-            />
-        )
-        uploaders.push(<VideoUpload videoName='' updateProfileData={this.updateForComponentId} />)
+  render() {
+    let uploaders = this.props.videos.map((x) => (
+      <VideoUpload
+        videoName={this.state.profileData.videoName}
+        updateProfileData={this.updateForComponentId}
+        saveVideoUrl={
+          "http://profile-service-talent.azurewebsites.net/profile/profile/updateTalentVideo"
+        }
+      />
+    ));
+    uploaders.push(
+      <VideoUpload videoName="" updateProfileData={this.updateForComponentId} />
+    );
 
-        return (
-            <React.Fragment>
-                {uploaders}
-            </React.Fragment>
-        )
-    }
+    return <React.Fragment>{uploaders}</React.Fragment>;
+  }
 }
